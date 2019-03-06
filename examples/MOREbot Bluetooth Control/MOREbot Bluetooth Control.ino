7b667d852a59dcbf1a337cab1f178d8a8fad3d35
MORERex <rex@moretech.co>
2019-03-05 23:12:09 -0600

    Updated example wording to fit Arduino style guide
    Signed-off-by: MORERex <rex@moretech.co>

--- examples/MOREbot Bluetooth Control/MOREbot Bluetooth Control.ino
+++ examples/MOREbot Bluetooth Control/MOREbot Bluetooth Control.ino
@@ -2,14 +2,14 @@
 #include <MOREbot.h>
 
 #define btname "MOREbot"
-#define LM 4
-#define RM 1
+#define left_Motor 4
+#define right_Motor 1
 #define trig 3
 #define echo 4
 #define rx 8
 #define tx 9
 
-MOREbot bot(btname, LM, RM, trig, echo, rx, tx);
+MOREbot bot(btname, left_Motor, right_Motor, trig, echo, rx, tx);
 
 void setup() {
   bot.setup();
